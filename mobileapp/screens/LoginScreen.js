33b0acd9376a176e9cbb014494702b578e3f067d
parthiv99 <parthadesai1999@gmail.com>
2021-10-10 16:40:35 -0700

    Parthiv
    Login

--- mobileapp/screens/LoginScreen.js
+++ mobileapp/screens/LoginScreen.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { AuthContext } from '../components/Context';
+import React, {Component} from 'react';
+import {View,Text,Image,TextInput,TouchableOpacity} from 'react-native';
+import {AuthContext} from '../components/Context';
+import {Button} from 'react-native';
+import styles from './styles';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 const LoginScreen = ({navigation}) => {
 	const { login } = React.useContext(AuthContext);
-
-	return (
-		<View style={styles.container}>
-			<Text>LOGIN SCREEN</Text>
-			<Button onPress={login} title="Login with GitHub" />
+		return(
+			<View style={{height:700,backgroundColor: "#1015A2"}}>
+					
+				<View style={styles.mainbody}>
+				<View>
+					<Image style = {styles.logo} source={require("../assets/titantalksLogo.png")}/>
+					</View>
+					<View style={styles.git}>
+					<TouchableOpacity
+				style={styles.gitstyle}
+				onPress={login}
+				>
+				<MaterialCommunityIcons name="github" color="white" size={30} /> 
+				<Text style={styles.gittext}> GitHub</Text>
+			</TouchableOpacity>
 		</View>
-	);
-}
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#b3e5fc',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
-
-export default LoginScreen;
+					<View>
+						<Text style={styles.labeluser}>
+							Username:
+							</Text>
+							<TextInput style={styles.input}
+							placeholder='20 characters max'/>
+												
+					</View>					
+					<View>
+						<Text style={styles.labelpass}>
+							Password
+							</Text>
+							<TextInput 
+							style={styles.pass}
+							secureTextEntry={true} autoCapitalize="none"
+							placeholder='Letters,numbers and special characters only'/>
+												
+					</View>
+					<View>
+					<Text style={styles.github}>
+					Log in With: </Text>
+					</View>
+					<View style= {{marginTop:-170}}>	
+					<Button  title="Log in"  type="clear"/>
+					</View>
+				</View>
+		</View>
+		)
+	}
+export default LoginScreen
---
+++ mobileapp/screens/styles.js
@@ -0,0 +1,79 @@
+const styles=
+{
+    mainbody: {
+        marginTop: 30,
+        marginLeft: 24,
+        marginRight: 24,
+        marginBottom: 120,
+
+    },
+    logo:{
+        
+        width: 150,
+        height: 140,
+        marginLeft:100,
+        marginTop:30
+    },
+
+    imglogin: {
+        width: 42,
+        height: 40,
+        marginLeft:160,
+        marginTop:450,
+    },
+    
+    labeluser:{
+        marginLeft:10,
+        fontSize: 18,
+        color: "#35D2EB",
+        marginTop:-350
+    },
+    input:{
+        width:300,
+        height:34,
+        borderWidth:1,
+        padding:8,
+        margin:10,
+        borderColor: "#35D2EB",
+        marginTop: 12
+    },
+    labelpass:{
+        marginLeft:10,
+        fontSize: 18,
+        color: "#35D2EB",
+        marginTop:-270
+    },
+    pass:{
+        width:300,
+        height:34,
+        borderWidth:1,
+        padding:8,
+        margin:10,
+        borderColor: "#35D2EB"
+    },
+    github:{
+        width:300,
+        height:34,
+        marginTop:-100,
+        fontSize:18
+    },
+    gitstyle:{
+        flexDirection: 'row',
+		
+		backgroundColor: '#424242',
+		borderWidth: 0.5,
+		borderColor: '#fff',
+		height: 37,
+		borderRadius: 5,
+		margin: 5,
+        marginTop:345
+    },
+    gittext:{
+        color: '#fff',
+		paddingLeft: 10,
+		paddingRight: 10,
+        paddingTop:7
+        
+    }
+}
+export default styles
