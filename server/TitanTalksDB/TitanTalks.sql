99b6c1c758e692ad030370ea1da1b055942865f9
nguyenly140 <nguyenly140@gmail.com>
2021-10-14 16:56:56 -0700

    Update TitanTalks.sql

--- server/TitanTalksDB/TitanTalks.sql
+++ server/TitanTalksDB/TitanTalks.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "User" (
 	"User_id"	TEXT NOT NULL UNIQUE,
-	"Username"	TEXT,
+	"Username"	TEXT NOT NULL,
 	"Fullname"	TEXT,
 	"Email"		TEXT,
 	"Avatar"	BLOB,
@@ -11,17 +11,19 @@ CREATE TABLE IF NOT EXISTS "User" (
 );
 CREATE TABLE IF NOT EXISTS "User_Post" (
 	"Post_id"	TEXT NOT NULL UNIQUE,
-	"User_id"	TEXT NOT NULL,
-	"Text"		TEXT,
-	"Post_datetime"	TEXT NOT NULL,
+	"Username"	TEXT NOT NULL,
+	"Body"		TEXT,
+	"Create_date"	TEXT NOT NULL,
+	"Modified_date"	TEXT,
+	"Views"		INTEGER
 	PRIMARY KEY("Post_id"),
 	FOREIGN KEY("User_id") REFERENCES "User"("User_id")
 );
 CREATE TABLE IF NOT EXISTS "Attachment" (
 	"Attach_id"	TEXT NOT NULL UNIQUE,
 	"Post_id"	TEXT NOT NULL,
-	"File_type"	TEXT NOT NULL,
-	"Directory"	TEXT,
+	"File_type"	BLOB NOT NULL,
+	"Created_date"	TEXT NOT NULL,
 	PRIMARY KEY("Attach_id"),
 	FOREIGN KEY("Post_id") REFERENCES "User_Post"("Post_id")
 );
