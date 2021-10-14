3881823a935206b991494881bbcc5cac46887f05
nguyenly140 <nguyenly140@gmail.com>
2021-10-14 16:29:54 -0700

    Update TitanTalks.sql
    Changes to table 'User':
    ADDED attribute User_id data-type(TEXT), Avatar data-type(BLOB), Bio data-type(TEXT),
    UPDATE combined Fname & Lname to Fullname,
    UPDATE User_id as PRIMARY KEY
    
    Changes to table 'User_post':,
    Updated attribute 'Username' = 'User_id',
    Updated foreign key from 'Username'='User_id'

--- server/TitanTalksDB/TitanTalks.sql
+++ server/TitanTalksDB/TitanTalks.sql
@@ -1,25 +1,27 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "User" (
-	"Username"	TEXT NOT NULL UNIQUE,
-	"Email"	TEXT NOT NULL,
-	"Fname"	TEXT NOT NULL,
-	"Lname"	TEXT NOT NULL,
-	"Sex"	TEXT,
-	PRIMARY KEY("Username")
+	"User_id"	TEXT NOT NULL UNIQUE,
+	"Username"	TEXT,
+	"Email"		TEXT,
+	"Fullname"	TEXT,
+	"Avatar"	BLOB,
+	"Bio"		TEXT,
+	"Sex"		TEXT,
+	PRIMARY KEY("User_id")
 );
 CREATE TABLE IF NOT EXISTS "User_Post" (
 	"Post_id"	TEXT NOT NULL UNIQUE,
-	"Username"	TEXT NOT NULL,
-	"Text"	TEXT NOT NULL,
+	"User_id"	TEXT NOT NULL,
+	"Text"		TEXT,
 	"Post_datetime"	TEXT NOT NULL,
 	PRIMARY KEY("Post_id"),
-	FOREIGN KEY("Username") REFERENCES "User"("Username")
+	FOREIGN KEY("User_id") REFERENCES "User"("User_id")
 );
 CREATE TABLE IF NOT EXISTS "Attachment" (
 	"Attach_id"	TEXT NOT NULL UNIQUE,
 	"Post_id"	TEXT NOT NULL,
 	"File_type"	TEXT NOT NULL,
-	"Directory"	TEXT NOT NULL,
+	"Directory"	TEXT,
 	PRIMARY KEY("Attach_id"),
 	FOREIGN KEY("Post_id") REFERENCES "User_Post"("Post_id")
 );
