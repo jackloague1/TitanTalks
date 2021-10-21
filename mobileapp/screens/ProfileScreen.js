0f751cf0b0f8d985e001dc4e9c212347917375ae
parthiv99 <parthadesai1999@gmail.com>
2021-10-21 08:50:07 -0700

    ProfilePageUpdated

--- mobileapp/screens/ProfileScreen.js
+++ mobileapp/screens/ProfileScreen.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button,Image } from 'react-native';
 import { AuthContext } from '../components/Context';
 import * as AuthHelper from '../utils/AuthHelper';
 
 const ProfileScreen = ({navigation}) => {
 	const { logout } = React.useContext(AuthContext);
-
+	
 	return (
-		<View style={styles.container}>
-			<Text>This is Profile Screen</Text>
-			<Button onPress={logout} title="Logout" />
-		</View>
-	);
-}
+		<View style={sty.container}>
+			<Image style = {sty.profImg} source={require("../assets/NguyenImg.png")}/>
+			<Image style = {sty.profLogo} source={require("../assets/titantalksLogo.png")}/>
+			<Text style={sty.profName}>Nguyen Nguyen</Text>
+			<Text style={sty.profID}>nguyen.nguyen</Text>
 
-const styles = StyleSheet.create({
+			<Text style={sty.profBio}>Stay motivated and Keep Inspiring</Text>	
+			<Text>
+				</Text>
+			<Button title ="Edit Profile"  type="clear"/>
+			<Text> 
+				</Text>
+			<Button title="Show my Achievements"  type="clear"/>
+			<Text> 
+				</Text>
+			<Button  title="Academic Progress"  type="clear"/>
+			<Text> 
+				</Text>
+			<Button  title="Recent discussions"  type="clear"/>
+			<Text> 
+				</Text>
+			<Button onPress={logout} title="Logout" />
+
+	</View>
+	)}
+
+
+
+const sty = StyleSheet.create({
+
 	container: {
 		flex: 1,
-		backgroundColor: '#c8e6c9',
+		backgroundColor: '#1015A2',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
-});
-
+	profLogo:{
+		width:40,
+		height:40,
+		position:'absolute'
+				
+	},
+	profID:{
+		flex:1,
+		marginTop:-280,
+		textAlignVertical: "top",
+		fontSize:20,
+		color:"#35D2EB",
+		marginLeft: -150,	
+	},
+	profImg:{
+		width: 200,
+		height:180,
+		
+		marginTop:110,
+		  
+	},
+	profName:{
+		fontSize:20,
+		color:"#35D2EB",
+		alignItems:'center'
+	},
+	profBio:{
+		marginBottom:30,
+		color: "#ffffff"
+	},
+})
 export default ProfileScreen;
